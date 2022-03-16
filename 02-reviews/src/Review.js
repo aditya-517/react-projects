44dc674840bebf2e17c8ca27ad95d2f2ebf97e76
Aditya Jadhav <83344372+aditya-517@users.noreply.github.com>
2022-03-16 12:04:32 +0530

    Add files via upload

---
+++ 02-reviews/src/Review.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      if (index === people.length - 1) return 0;
+
+      return index + 1;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      if (index === 0) return people.length - 1;
+
+      return index - 1;
+    });
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Review;
